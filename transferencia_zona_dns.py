2cd7bda4f42691b03f1b4a5763e3d35ecf68eb60
Eduardo Cezar da Silva <82180468+eduardocs2001@users.noreply.github.com>
2022-08-22 23:18:34 -0300

    Add files via upload

---
+++ transferencia_zona_dns.py
@@ -0,0 +1,30 @@
+import subprocess
+
+class transferencia_zona:
+    
+    def __init__(self):
+        self.__dns: str(input("DNS: "))
+        
+    def transferencia(self):
+        name_servers = (subprocess.getoutput(f'host -t ns {dns} | cut -d " " -f 4')).split("\n")
+        for x in name_servers:
+            transeferencia = subprocess.getoutput(f'host -l -a {dns} {x}')
+            if "Transfer failed." not in transeferencia:
+                print(f"Servidor: {x}\n{transeferencia}")
+            else: 
+                print(f"Transferência de zona para o servidor {x} foi recusada")
+    
+
+dns = input("DNS:")
+name_servers = (subprocess.getoutput(f'host -t ns {dns} | cut -d " " -f 4')).split("\n")
+for x in name_servers:
+    transeferencia = subprocess.getoutput(f'host -l -a {dns} {x}')
+    if "Transfer failed." not in transeferencia:
+        print(f"Servidor: {x}\n{transeferencia}")
+    else: 
+        print(f"Transferência de zona para o servidor {x} foi recusada")
+        
+        
+if __name__ == '__main__':
+    script = transferencia_zona()
+    script.transferencia()
